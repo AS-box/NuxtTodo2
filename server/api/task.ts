c451d5103755a7cb9ae312616dc304d8c27b99cb
ai-tanaka629 <ai.tanaka@webcrew.co.jp>
2025-02-27 17:05:06 +0900

    import修正

--- server/api/task.ts
+++ server/api/task.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from '@prisma/extension-accelerate'
+
 
 export default defineEventHandler(async () => {
-    const prisma = new PrismaClient().$extends(withAccelerate())
+    const prisma = new PrismaClient()
     const tasks = await prisma.task.findMany();
     return tasks;
 });
--- server/api/tasks.delete.ts
+++ server/api/tasks.delete.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from '@prisma/extension-accelerate'
+
 
 export default defineEventHandler(async (event) => {
-    const prisma = new PrismaClient().$extends(withAccelerate())
+    const prisma = new PrismaClient()
     const body = await readBody(event);
     const deleteTask = await prisma.task.delete({
         where:{
